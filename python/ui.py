be39eb49ca72f63ae57e0c75a7d11fd983ade1e2
Nicolas Janis <Nicolas.d.janis@gmail.com>
2023-02-04 17:26:38 -0500

    update: UI for new languages

--- python/ui.py
+++ python/ui.py
@@ -10,11 +10,19 @@ class Ui():
 
         self._languagesDict = {
             'untranslatedText': StringVar(),
-            'spanishText': StringVar(),
-            'portugueseText': StringVar(),
-            'italianText': StringVar(),
-            'frenchText': StringVar(),
-            'romanianText': StringVar()
+            'translated1Text': StringVar(),
+            'translated2Text': StringVar(),
+            'translated3Text': StringVar(),
+            'translated4Text': StringVar(),
+            'translated5Text': StringVar()
+        }
+        
+        self._translationCodes = {
+            'translationCode1': StringVar(),
+            'translationCode2': StringVar(),
+            'translationCode3': StringVar(),
+            'translationCode4': StringVar(),
+            'translationCode5': StringVar(),
         }
 
         self._createUiWindow()
@@ -38,100 +46,111 @@ class Ui():
         ).place(x=250, y=93)
 
         #Create English Input Box
-        self._createLabel("English", 130)
-        Entry(self._master, textvariable=self._languagesDict['untranslatedText']).place(x=self._ENTRY_X_POS, y=130)
+        Label(self._master, text="English", width=20, font=("bold", 10)).place(x=self._LABEL_X_POS, y=130)
+        Entry(self._master, textvariable=self._languagesDict['untranslatedText']).place(x=self._ENTRY_X_POS, y=130, width=230)
 
-        # Create Spanish Boxes
-        self._createLabel("Spanish", 160)
+        # Create Language 1 Boxes
+        self._createTranslateCodeEntry("", "translationCode1", 160)
         Entry(
             self._master,
-            textvariable=self._languagesDict['spanishText'],
+            textvariable=self._languagesDict['translated1Text'],
             state=DISABLED
-        ).place(x=self._ENTRY_X_POS, y=160)
+        ).place(x=self._ENTRY_X_POS, y=160, width=230)
 
-        # Create Portuguese Boxes
-        self._createLabel("Portuguese", 190)
+        # Create Language 2 Boxes
+        self._createTranslateCodeEntry("", "translationCode2", 190)
         Entry(
             self._master,
-            textvariable=self._languagesDict['portugueseText'],
+            textvariable=self._languagesDict['translated2Text'],
             state=DISABLED
-        ).place(x=self._ENTRY_X_POS, y=190)
+        ).place(x=self._ENTRY_X_POS, y=190, width=230)
 
-        # Create Italian Boxes
-        self._createLabel("Italian", 220)
+        # Create Language 3 Boxes
+        self._createTranslateCodeEntry("", "translationCode3", 220)
         Entry(
             self._master,
-            textvariable=self._languagesDict['italianText'],
+            textvariable=self._languagesDict['translated3Text'],
             state=DISABLED
-        ).place(x=self._ENTRY_X_POS, y=220)
+        ).place(x=self._ENTRY_X_POS, y=220, width=230)
 
-        # Create French Boxes
-        self._createLabel("French", 250)
+        # Create Language 4 Boxes
+        self._createTranslateCodeEntry("", "translationCode4", 250)
         Entry(
             self._master,
-            textvariable=self._languagesDict['frenchText'],
+            textvariable=self._languagesDict['translated4Text'],
             state=DISABLED
-        ).place(x=self._ENTRY_X_POS, y=250)
+        ).place(x=self._ENTRY_X_POS, y=250, width=230)
 
-        # Create Romanian Boxes
-        self._createLabel("Romanian", 280)
+        # Create Language 5 Boxes
+        self._createTranslateCodeEntry("", "translationCode5", 280)
         Entry(
             self._master,
-            textvariable=self._languagesDict['romanianText'],
+            textvariable=self._languagesDict['translated5Text'],
             state=DISABLED
-        ).place(x=self._ENTRY_X_POS, y=280)
+        ).place(x=self._ENTRY_X_POS, y=280, width=230)
         
 
         # Create translate button
-        Button(self._master, text="Translate", command=self._translate).place(x=370, y=310)
+        self._master.bind('<Return>', self._translate)
+        Button(self._master, text="Translate").place(x=370, y=310)
+        self._master.bind('<Button-1>', self._translate)
 
-    def _createLabel(self, displayText:str, y_pos: int) -> Label:
-        return Label (
+    def _createTranslateCodeEntry(self, displayText: str, translationCodeEntry: str, y_pos: int) -> Entry:
+        self._translationCodes[translationCodeEntry].set(displayText)
+        return Entry (
             self._master,
-            text=displayText,
+            textvariable=self._translationCodes[translationCodeEntry],
+            justify=CENTER,
             width=20,
             font=("bold", 10)
         ).place(x=self._LABEL_X_POS, y=y_pos)
 
-    def _translate(self) -> None:
+    def _translate(self, event) -> None:
+        translations = {
+                'translation1': '',
+                'translation2': '',
+                'translation3': '',
+                'translation4': '',
+                'translation5': ''
+        }
+
         if self._languagesDict['untranslatedText'] != '':
-            spanishTranslation = Translate(
-                self._languagesDict['untranslatedText'].get(),
-                'es'
-            )
-            portugueseTranslation = Translate(
-                self._languagesDict['untranslatedText'].get(),
-                'pt'
-            )
-            italianTranslation = Translate(
-                self._languagesDict['untranslatedText'].get(),
-                'it'
-            )
-            frenchTranslation = Translate(
-                self._languagesDict['untranslatedText'].get(),
-                'fr'
-            )
-            romanianTranslation = Translate(
-                self._languagesDict['untranslatedText'].get(),
-                'ro'
-            )
+            if self._translationCodes['translationCode1'].get() != '':
+                translation1 = Translate(
+                    self._languagesDict['untranslatedText'].get(),
+                    self._translationCodes['translationCode1'].get().lower()
+                )
+                translations['translation1'] = translation1.getTranslatedText()
+            if self._translationCodes['translationCode2'].get() != '':
+                translation2 = Translate(
+                    self._languagesDict['untranslatedText'].get(),
+                    self._translationCodes['translationCode2'].get().lower()
+                )
+                translations['translation2'] = translation2.getTranslatedText()
+            if self._translationCodes['translationCode3'].get() != '':
+                translation3 = Translate(
+                    self._languagesDict['untranslatedText'].get(),
+                    self._translationCodes['translationCode3'].get().lower()
+                )
+                translations['translation3'] = translation3.getTranslatedText()
+            if self._translationCodes['translationCode4'].get() != '':
+                translation4 = Translate(
+                    self._languagesDict['untranslatedText'].get(),
+                    self._translationCodes['translationCode4'].get().lower()
+                )
+                translations['translation4'] = translation4.getTranslatedText()
+            if self._translationCodes['translationCode5'].get() != '':
+                translation5 = Translate(
+                    self._languagesDict['untranslatedText'].get(),
+                    self._translationCodes['translationCode5'].get().lower()
+                )
+                translations['translation5'] = translation5.getTranslatedText()
 
-            translations = {
-                'spanish': spanishTranslation.getTranslatedText(),
-                'portuguese': portugueseTranslation.getTranslatedText(),
-                'italian': italianTranslation.getTranslatedText(),
-                'french': frenchTranslation.getTranslatedText(),
-                'romanian':romanianTranslation.getTranslatedText()
-            }
-
-            self._languagesDict['spanishText'].set(translations['spanish'])
-            self._languagesDict['portugueseText'].set(translations['portuguese'])
-            self._languagesDict['italianText'].set(translations['italian'])
-            self._languagesDict['frenchText'].set(translations['french'])
-            self._languagesDict['romanianText'].set(translations['romanian'])
-
-
-                
+            self._languagesDict['translated1Text'].set(translations['translation1'])
+            self._languagesDict['translated2Text'].set(translations['translation2'])
+            self._languagesDict['translated3Text'].set(translations['translation3'])
+            self._languagesDict['translated4Text'].set(translations['translation4'])
+            self._languagesDict['translated5Text'].set(translations['translation5'])         
 
     def runMainLoop(self) -> None:
         self._master.mainloop()
